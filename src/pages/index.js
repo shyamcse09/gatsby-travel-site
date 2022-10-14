f77458bd8a5787832b3f78ec48d24f4b2d1511a1
Mondal,Shyam <smonda@ITS.JNJ.com>
2022-10-13 21:39:17 -0400

    Markdown update

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Seo from "../components/metadata"
 export default function Home() {
   return (
     <div>
-      <Seo title="Home Page" />
+      <Seo title="Home Page" description="Home Page content For Travel Guide site"/>
       <Layout>
         <div className="home-carousel max-w-screen-xl mx-auto">
           <SliderImageCarousel />
